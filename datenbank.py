5e74c44f18573f6607d30c701dfd9e02513bb2b1
franceskn <frances.knoepfel@stud.fhgr.ch>
2023-05-19 16:49:25 +0200

    19.5.

--- datenbank.py
+++ datenbank.py
@@ -1,43 +1,16 @@
-from json import dumps, loads
-
+import json
 
 def read_json(file_name):
-    with open(file_name, encoding="utf8") as open_file:
-        content = open_file.read()
-        json_content = loads(content)
-    tasks = json_content.get('tasks', [])
-    return tasks
-
-
+    try:
+        with open(file_name, 'r') as f:
+            json_content = json.load(f)
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        json_content = []
+    return json_content
 
 def write_json(file_name, inhalt):
     json_inhalt = read_json(file_name)
     json_inhalt.append(inhalt)
     with open(file_name, "w", encoding="utf8") as open_file:
-        json_str = dumps(json_inhalt, indent=4)
+        json_str = json.dumps(json_inhalt, indent=4)
         open_file.write(json_str)
-
-
-tasks = read_json('tasks.json')
-neuer_task = {
-    "name": "Staubsaugen",
-    "deadline": "01.07.2023",
-    "priority": "mittel",
-    "category": "Haushalt"
-}
-write_json('tasks.json', neuer_task)
-
-
-tasks.append(neuer_task)
-write_json('tasks.json', tasks)
-
-
-kategorie = read_json('kategorie.json')
-neue_kategorie = {
-    "name": "Haushalt"
-}
-kategorie = {neue_kategorie['name']: neue_kategorie}
-write_json('kategorie.json', kategorie)
-
-
-
--- main.py
+++ main.py
@@ -9,66 +9,38 @@ tasks = []
 # list of lists
 lists = read_json('lists.json')
 
+def get_list_names():
+    lists = read_json('lists.json')
+    return [lst['name'] for lst in lists]
+
 @app.route('/')
 def index():
-    return render_template('index.html', tasks=tasks, lists=lists)
+    list_names = get_list_names()  # Retrieve list names
+    return render_template('index.html', tasks=tasks, lists=lists, list_names=list_names)
 
 # add a new task
 @app.route('/add_task', methods=['POST'])
 def add_task():
-    task = {'name': request.form['task_name'], 'deadline': request.form['deadline'],
-            'priority': request.form['priority'], 'list_name': request.form['list_name']}
+    list_names = get_list_names()  # Retrieve list names
+    task = {
+        'name': request.form['task_name'],
+        'deadline': request.form['deadline'],
+        'priority': request.form['priority'],
+        'list_name': request.form['list_name']
+    }
     tasks.append(task)
-    return render_template('index.html', tasks=tasks, lists=lists)
+    return render_template('index.html', tasks=tasks, lists=lists, list_names=list_names)
 
 # add a new list
 @app.route('/add_list', methods=['POST'])
 def add_list():
-    list = {'name': request.form['list_name'], 'description': request.form['list_description']}
-    lists.append(list)
+    list_name = request.form['list_name']
+    list_description = request.form['list_description']
+    new_list = {'name': list_name, 'description': list_description}
+    lists.append(new_list)
     write_json('lists.json', lists)
-    return render_template('index.html', tasks=tasks, lists=lists)
-
-# sort by priority
-@app.route('/sort_priority', methods=['POST'])
-def sort_priority():
-    tasks_sorted = sorted(tasks)
-    return render_template('index.html', tasks=tasks_sorted, lists=lists)
-
-@app.route("/neue_kategorie", methods=["GET", "POST"])
-def neue_kategorie():
-    if request.method == "GET":
-        return render_template("neue_kategorie.html")
-
-    if request.method == "POST":
-        kategorie_name = request.form['kategorie_name']
-        priority = request.form['priority']
-        neue_kategorie = {
-            "name": kategorie_name,
-            "priority": priority
-        }
-        write_json('kategorie.json', neue_kategorie)
-        return "Kategorie wurde gespeichert"
-
-
-@app.route("/lists")
-def get_lists():
-    return render_template('task_liste.html', lists=lists)
-
-@app.route("/neuer_task", methods=["GET", "POST"])
-def neuer_task():
-    if request.method == "GET":
-        return render_template("neuer_task.html")
-
-    if request.method == "POST":
-        task_name = request.form['task_name']
-        priorität = request.form['priorität']
-        neuer_task = {
-            "name": task_name,
-            "priorität": priorität
-        }
-        write_json('tasks.json', neuer_task)
-        return "Task wurde gespeichert"
+    list_names = get_list_names()  # Retrieve list names
+    return render_template('index.html', tasks=tasks, lists=lists, list_names=list_names)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
